66a83c0da7cb3e503db0eac193eb450923210604
Your Name <you@example.com>
2019-10-29 19:35:06 +0530

    questions updated

--- 01BitwiseOperation/evenOdd.c
+++ 01BitwiseOperation/evenOdd.c
@@ -3,6 +3,8 @@
 int evenOdd (int num)
 {
 	// write your code here
+	// return 1 if number is even 
+	// return 0 if number is odd
 
-	return num;
+	return 1;
 }
--- 01BitwiseOperation/main.c
+++ 01BitwiseOperation/main.c
@@ -6,5 +6,42 @@ int main()
 	// write your code here
 	// use switch keyword to prompt the user 
 	//
+	
+	int num, choice, result;
+
+	printf("1. check number is even or odd. \n");
+	printf("2. check number is power of 2. \n");
+	printf("3. count the number of set bit of a number. \n");
+	printf("4. toggle odd bits of a number \n");
+	printf("5. set a bit at given position. \n");
+
+	printf("Enter your choice: ");
+	scanf("%d", &choice);
+	
+	printf("Enter the number: ");
+	scanf("%d", &num);
+
+	switch(choice){
+		case 1:
+			result = evenOdd(num);
+			if(result == 1){
+				printf("%d is even\n", num);
+			}
+			else{
+				printf("%d is odd\n", num);
+			}
+		break;
+
+		case 2:
+
+		break;
+		
+		// complete other cases 
+
+		default:
+			printf("Wrong choice \n");
+		break;
+	}
+
 	return 0;
 }
