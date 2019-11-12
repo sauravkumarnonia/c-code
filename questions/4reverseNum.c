f03e8d03b050d660ec2bd862c3e552a6b6a8b30b
Your Name <you@example.com>
2019-11-12 22:53:45 +0530

    format added

---
+++ questions/4reverseNum.c
@@ -0,0 +1,28 @@
+/* ************************************************************************
+ * Created by    : Rahul Kumar Nonia
+ * File name     : 4reverseNum.c
+ * Created on    : Tuesday 12 November 2019 10:48:36 PM IST
+ * Last modified : Tuesday 12 November 2019 10:53:02 PM IST
+ * Description   : 
+ * ***********************************************************************/
+
+#include <stdio.h>
+int reverseNum(int);
+
+int main()
+{
+	int num, result;
+	printf("Enter the number: ");
+	scanf("%d", &num);
+	
+	result = reverseNum(num);
+    printf("Number after reversing: %d\n", result);
+	return 0;
+}
+
+int reverseNum(int num)
+{
+
+	// write your code
+	return  reverse; // return reversed number
+}
---
+++ questions/5printPrime.c
@@ -0,0 +1,20 @@
+/* ************************************************************************
+ * Created by    : Rahul Kumar Nonia
+ * File name     : 5printPrime.c
+ * Created on    : Tuesday 12 November 2019 10:46:16 PM IST
+ * Last modified : Tuesday 12 November 2019 10:48:23 PM IST
+ * Description   : 
+ * ***********************************************************************/
+
+#include <stdio.h>
+
+void printPrime ()
+{
+	// code to print first 10 prime numbers
+}
+
+int main()
+{
+	printPrime();
+	return 0;
+}
