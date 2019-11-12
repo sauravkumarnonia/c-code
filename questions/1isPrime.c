a659d1ea4c0f15ee3633581428420800aded8ef5
Your Name <you@example.com>
2019-11-12 21:30:37 +0530

    smaple

---
+++ questions/1isPrime.c
@@ -0,0 +1,10 @@
+/* ************************************************************************
+ * Created by    : Rahul Kumar Nonia
+ * File name     : 1isPrime.c
+ * Created on    : Tuesday 12 November 2019 09:28:56 PM IST
+ * Last modified : Tuesday 12 November 2019 09:29:15 PM IST
+ * Description   : 
+ * ***********************************************************************/
+
+
+// write your codes
