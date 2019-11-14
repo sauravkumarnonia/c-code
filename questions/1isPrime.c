9b62c55618cdcf4936bf0fec99709ac37cfa635e
Your Name <you@example.com>
2019-11-14 20:53:47 +0530

    questions

--- questions/1isPrime.c
+++ questions/1isPrime.c
@@ -2,33 +2,34 @@
  * Created by    : Rahul Kumar Nonia
  * File name     : 1isPrime.c
  * Created on    : Tuesday 12 November 2019 09:28:56 PM IST
- * Last modified : Tuesday 12 November 2019 09:29:15 PM IST
+ * Last modified : Thursday 14 November 2019 08:49:11 PM IST
  * Description   : 
  * ***********************************************************************/
 
 
 // write your codes
 #include<stdio.h>
-int i,num;
-void isPrime(int){
 
-
-printf("Enter a number:");
-scanf("%d",&num);
-for(i=2; i<=num-1; i++)
-	if(num%i == 0)
-		break;
-if(i == num)
-
-	printf("%d is a Prime number",num);
-else
-	printf("%d is not a Prime number",num);
-
+int isPrime(int num){
+	int i;
+	for(i = 2; i <= num / 2; i++){
+		if (num % i == 0){
+			return 0;
+		}
+	}
+	return 1;
 }
-int main(){
 
-isPrime(num);
-return 0;
+int main(){
+	int num;
+	printf("Enter number: ");
+	scanf("%d", &num);
+	if (isPrime(num))
+		printf("%d is prime number\n", num);
+	else
+		printf("%d is not a prime number\n", num);
+
+	return 0;
 }
 
 
